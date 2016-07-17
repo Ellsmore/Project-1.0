fa78d14a3a2ca6bbf21c0f0b658f9d6070a4952f
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-07-17 19:00:03 +1000

    Updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,31 @@
 Client.destroy_all
-c1 = Client.create(:business_name => "Badgers Chickens", :address => "12 smith road", :suburb => "Bowral", :contact_name => "Chris Badger")
-
-c2 = Client.create(:business_name => "Badgers Chickens", :address => "12 smith road", :suburb => "Bowral", :contact_name => "Chris Badger")
-
---------------------------------------------------------------------------------
-
-JobAd.destroy_all
-j1 = JobAd.create(:title => "Chicken labourers needed", :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-
-j2 = JobAd.create(:title => "Conreters needed", :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-
---------------------------------------------------------------------------------
-
-Application.destroy_all
-a1 = Application.create(:accepted => "yes", :short_list => "yes", :description => "this bloke goes alright")
-
-a2 = Application.(:accepted => "no", :short_list => "yes", :description => "this bloke goes alright, but could be better")
-
-a3 = Application.create(:accepted => "no", :short_list => "no", :description => "This bloke is terrible")
-
---------------------------------------------------------------------------------
-
 Candidate.destroy_all
-ca1 = Candidate.create(:first_name => "John", :last_name => "Smith", :email => "johnsmith@gmail.com.au")
+JobAd.destroy_all
+Application.destroy_all
+User.destroy_all
 
-ca2 = Candidate.create(:first_name => "chris", :last_name => "ellsmore", :email => "chrisellsmore@gmail.com.au")
+User.create(:name => "Admin", :password_digest => "chicken", :age => 32, :email => "admin123@gmail.com")
 
-ca3 = Candidate.create(:first_name => "Jordan", :last_name => "Berta", :email => "jordanberta@gmail.com.au")
+
+Client.create(:business_name => "Badgers Chickens", :address => "12 smith road", :suburb => "Bowral", :contact_name => "Chris Badger")
+
+Client.create(:business_name => "Badgers Chickens", :address => "12 smith road", :suburb => "Bowral", :contact_name => "Chris Badger")
+
+
+JobAd.create(:title => "Chicken labourers needed", :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+JobAd.create(:title => "Conreters needed", :description => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+
+Application.create(:accepted => "yes", :short_list => "yes", :description => "this bloke goes alright")
+
+Application.(:accepted => "no", :short_list => "yes", :description => "this bloke goes alright, but could be better")
+
+Application.create(:accepted => "no", :short_list => "no", :description => "This bloke is terrible")
+
+
+Candidate.create(:first_name => "John", :last_name => "Smith", :email => "johnsmith@gmail.com.au")
+
+Candidate.create(:first_name => "chris", :last_name => "ellsmore", :email => "chrisellsmore@gmail.com.au")
+
+Candidate.create(:first_name => "Jordan", :last_name => "Berta", :email => "jordanberta@gmail.com.au")
